d5b3bed3d2be2edec9b369e2a784678eb8e93b80
zayceva-nastya <59017854+zayceva-nastya@users.noreply.github.com>
2020-01-17 21:14:07 +0300

    Create array_table.php
    17.01

---
+++ array_table.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+$array=array(
+   1=>'Иванов',
+   2=>'Петров',
+   3=>'Сидоров',
+   4=>'Смирнов',
+   5=>'Соболев');
+  
+function tb($array)
+ $str ="<table>";
+{
+   foreach()
+ 
+}
+  
+  
+
+
+   ?>
+
+    
+   
+</body>
+</html>
