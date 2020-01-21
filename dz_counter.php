ee0797ca4c4ec9e6205bea568cb719ff8f750caa
zayceva-nastya <59017854+zayceva-nastya@users.noreply.github.com>
2020-01-21 21:15:52 +0300

    Create dz_counter.php
    lesson

---
+++ dz_counter.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    $fp=fopen ("counter.txt","w+");
+    fwrite ($fp, "0");
+    fclose ($fp);
+
+    $file=('counter.txt');
+    readfile($file);
+
+    $sa=fopen("counter.txt","r+");
+    
+    ?>
+    
+</body>
+</html>
