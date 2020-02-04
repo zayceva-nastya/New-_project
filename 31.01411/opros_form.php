d7f583775c4fb7a228ba5692512c37e443d0a636
zayceva-nastya <59017854+zayceva-nastya@users.noreply.github.com>
2020-02-04 15:16:14 +0300

    Create opros_form.php

---
+++ 31.01411/opros_form.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="opros_forma.css">
+</head>
+
+<body>
+    <form action="opros1.php" method="post">
+        <?php
+        $arr = file('opros.csv');
+        ?>
+        <h3><?= $arr[0] ?></h3>
+        <?= "<table>\n" ?>
+        <?php
+        for ($i = 1; $i < count($arr); $i++) {
+            $buf = explode(" - ", $arr[$i])[0];
+            echo "<tr><td><input class='radio_input' id='radio'  type='radio' name='vot'  value='$i'><label for='radio'>$buf</label></td></tr>\n";
+        }
+
+        ?>
+        <?= "</table>\n" ?>
+        <input id="vote" type="submit" value="VOTE">
+    </form>
+</body>
+
+</html>
