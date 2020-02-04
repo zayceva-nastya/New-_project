e0bac05b07dd089169eea6a7428c50e7fc9c7f21
zayceva-nastya <59017854+zayceva-nastya@users.noreply.github.com>
2020-02-04 21:29:18 +0300

    Create chat1.php

---
+++ 04.02/chat1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="0.5" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="1.css">
+</head>
+
+<body>
+
+    <?php
+  
+    $ind=file('chat.txt');
+    foreach($ind as $key=>$value){
+        echo "<table><tr><td>". $value. "</table></tr></td><br>";
+    }
+    
+    
+    ?>
+    
+</body>
+</html>
