d36f794c6193b8108b42daaa64293626ea186e0a
zayceva-nastya <59017854+zayceva-nastya@users.noreply.github.com>
2020-02-11 18:43:24 +0300

    Create cache.php

---
+++ HTTP/cache.php
@@ -0,0 +1,5 @@
+<?php //запрет кеширования
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-cache, must-revalidate");
+?>
