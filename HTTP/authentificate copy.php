7fe4116dce1dd2a69f133f2826c3136f3073c855
zayceva-nastya <59017854+zayceva-nastya@users.noreply.github.com>
2020-02-11 18:43:19 +0300

    Create authentificate copy.php

---
+++ HTTP/authentificate copy.php
@@ -0,0 +1,12 @@
+<?php
+if (!isset($_SERVER['PHP_AUTH_USER'])) {
+    header('WWW-Authenticate: Basic realm="My Realm"');
+    header('HTTP/1.0 401 Unauthorized');
+    echo 'Текст, отправляемый в том случае,
+    если пользователь нажал кнопку Cancel';
+    exit;
+} else {
+    echo "<p>Hello {$_SERVER['PHP_AUTH_USER']}.</p>";
+    echo "<p>Вы ввели пароль {$_SERVER['PHP_AUTH_PW']}.</p>";
+}
+?>
