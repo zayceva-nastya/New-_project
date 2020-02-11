ee5e296c731f140c6ffe571d6841d8a351b6ad33
zayceva-nastya <59017854+zayceva-nastya@users.noreply.github.com>
2020-02-11 18:43:50 +0300

    Create sam.php

---
+++ HTTP/sam.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="sam.css">
+</head>
+<body>
+    <?php
+print_r($_GET);
+echo "<div class='pink' style=' height: <?=$_GET['a']?> '></div>"
+
+?>
+</body>
+</html>
+
