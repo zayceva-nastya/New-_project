b918118ce43cf6bc0429c1463812fc25ef6bf271
zayceva-nastya <59017854+zayceva-nastya@users.noreply.github.com>
2020-02-11 18:43:29 +0300

    Create 1.php

---
+++ HTTP/calc/1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    print_r($_POST);
+    print_r($_GET);
+    
+    ?>
+    
+</body>
+</html>
